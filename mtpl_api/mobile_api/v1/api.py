a82288895e5de504b55b62500e4936b2932bb6ca
Midocean <manjeetkhatri008@gmail.com>
2023-10-05 17:10:12 +0530

    changes

--- mtpl_api/mobile_api/v1/api.py
+++ mtpl_api/mobile_api/v1/api.py
@@ -19,8 +19,15 @@ def login(usr, pwd):
         # validate_employee(login_manager.user)
         login_manager.post_login()
         if frappe.response["message"] == "Logged In":
+            if not frappe.db.exists("Smart Connect User", login_manager.user):
+                return gen_response(500, "User has no permission for mobile app, Please Contect Admin")
+            
+            if frappe.get_value("Smart Connect User", login_manager.user, "enable") == 0:
+                return gen_response(500, "User has no permission for mobile app, Please Contect Admin")
+            # if frappe.get_doc("Smart Connect User", login_manager.user)
             frappe.response["user"] = login_manager.user
             frappe.response["key_details"] = generate_key(login_manager.user)
+            frappe.response["smart_user_details"] = frappe.get_doc('Smart Connect User', login_manager.user)
             frappe.response["user_details"] = frappe.get_doc('User', login_manager.user)
         gen_response(200, frappe.response["message"])
     except frappe.AuthenticationError:
